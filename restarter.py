e8eb9c469c8d4a229a4945356eabb8fca815d216
ghostdragons445 <ghostdragons445@gmail.com>
2018-11-05 19:49:39 +0000

    add zipfile

--- restarter.py
+++ restarter.py
@@ -20,45 +20,51 @@ class ParseConf:
     custom_dir = parser.get('Sync files path', 'Custom path')
     readme_dir = parser.get('Sync files path', 'Readme Path')
     server_dir = parser.get('Server location', 'Server Path')
+    zip_log_en = parser.getboolean('Archive', 'Archive server log')
+    zip_log_path = parser.get('Archive', 'Server log path')
+    zip_log_ignore = parser.get('Archive', 'log to leave')
+
+
 
 
 
 # TODO:Testing
 # Check if server is running
 
-class ServerCheck:
+def ServerCheck():
     print('Checking if server already exists...')
     if psutil.pid_exists(ParseConf.arma_ser_pid):
         print("closing existing server...")
         os.popen('TASKKILL /PID ' + str(ParseConf.arma_ser_pid) + ' /F')
+        BecCheck()
     else:
-        StartSequence()
+        BecCheck()
 
 
 
 
 # TODO:Testing
 # Check if BEC is running
-class BecCheck:
+def BecCheck:
     print('Checking if BEC already exists...')
     if psutil.pid_exists(ParseConf.bec_pid):
         print("closing existing BEC instance...")
         os.popen('TASKKILL /PID ' + str(ParseConf.bec_pid) + ' /F')
-        ServerCheck
+        HeadlessCheck()
     else:
-        ServerCheck
+        HeadlessCheck()
 
 
 # TODO:Testing
 # Check if headless client is running
-class HeadlessCheck:
+def HeadlessCheck:
     print('Checking if Headless client already exists...')
     if psutil.pid_exists(ParseConf.hc_pid):
         print("closing existing Headless client...")
         os.popen('TASKKILL /PID ' + str(ParseConf.hc_pid) + ' /F')
-        BecCheck
+        StartSequence()
     else:
-        BecCheck
+        StartSequence()
 
 
 # TODO:code to check for steam mod updates and copy files needed
@@ -111,16 +117,32 @@ class CopyMissions:
 class CopyBeServer:
     dest_dir = ParseConf.server_dir  # Edit to BEserver.cfg location
     shutil.copy(ParseConf.mpmissions_dir, dest_dir)
+
+
 # archive server logs
 # investigate usage of zipfile
 class ZipLog:
-    if ParseConf.zip_log_en = True:
+    log_paths = []
+    sorted_log_paths = []
+    if ParseConf.zip_log_en:
+        for file in os.listdir(ParseConf.zip_log_path):
+            if file.endswith('.log'):
+                log_path = ParseConf.zip_log_path + file
+                log_paths.append(log_path)
+                sorted_log_paths = sorted(log_paths, key=os.path.getctime)
+                for x in range(len(sorted_log_path)):
+                    if x >= ParseConf.zip_log_ignore:
+                        with zipfile.ZipFile('my_python_files.zip', 'w') as zip:
+                            # writing each file one by one
+                            zip.write(sorted_log_paths[x])
 
 
 
-# archive BEC logs
 
-# Back up optionsz
+
+            # archive BEC logs
+
+# Back up options
 
 # TODO: Steam check for server update
 
